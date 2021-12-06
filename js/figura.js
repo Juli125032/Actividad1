4c0f9d7aebc40557ba7be335f4ea64dd695bf503
Juliana Deantonio <juliana.deantonio@gluky.com>
2021-12-06 13:39:15 -0500

    fin

--- js/figura.js
+++ js/figura.js
@@ -1,11 +1,27 @@
 class Figura {
 
+    constructor(figura) {
+        this.figura = figura;
+    }
+
     desplazar(){
+        let margin = this.figura.style.marginLeft;
+        if(margin){
+            margin = margin.split('px')[0];
+            this.figura.style.marginLeft = `${parseInt(margin)+15}px`;
+            return;
+        }
+        this.figura.style.marginLeft = `15px`;
 
     }
 
-    calcularArea(){
+    restaurar(){
+        this.figura.style.marginLeft = `0px`;
+    }
 
+
+    calcularArea(base, altura){
+        return base * altura;
     }
 }
 
@@ -16,22 +32,61 @@ class Circulo extends Figura {
     }
 
     calcularArea(){
-        let area = 2.14 * (this.radio * this.radio);
+        let area = 3.14 * (this.radio * this.radio);
         const circle = document.querySelector(".circle");
         circle.style.width = `${area}px`;
         circle.style.height = `${area}px`;
         circle.style.borderRadius = `${area}px`;
+        document.querySelector("#resultAreaCircle").innerHTML = `Area: ${area}`
     }
 }
 
-class Triángulo extends Figura {
+class Triangulo extends Figura {
+    constructor(base, altura) {
+        super();
+        this.base = base;
+        this.altura = altura;
+    }
 
+    calcularArea(){
+        let areaForma = new Figura(document.querySelector(".triangulo")).calcularArea(this.base, this.altura);
+        let area = areaForma / 2;
+        const triangulo = document.querySelector(".triangulo");
+        triangulo.style.width = `${this.base}px`;
+        triangulo.style.height = `${this.altura}px`;
+        document.querySelector("#resultAreaTriangulo").innerHTML = `Area: ${area}`
+    }
 }
 
-class Rectángulo extends Figura {
+class Rectangulo extends Figura {
+    constructor(base, altura) {
+        super();
+        this.base = base;
+        this.altura = altura;
+    }
 
+    calcularArea(){
+        let area = new Figura(document.querySelector(".rectangulo")).calcularArea(this.base, this.altura);
+        const rectangulo = document.querySelector(".rectangulo");
+        rectangulo.style.width = `${this.base}px`;
+        rectangulo.style.height = `${this.altura}px`;
+        document.querySelector("#resultAreaRectangulo").innerHTML = `Area: ${area}`
+    }
 }
 
 class Rombo extends Figura {
+    constructor(diagonalMenor, diagonalMayor) {
+        super();
+        this.diagonalMenor = diagonalMenor;
+        this.diagonalMayor = diagonalMayor;
+    }
 
+    calcularArea(){
+        let areaForma = new Figura(document.querySelector(".rombo")).calcularArea(this.diagonalMenor, this.diagonalMayor);
+        let area = areaForma / 2;
+        const rombo = document.querySelector(".rombo");
+        rombo.style.width = `${this.diagonalMenor}px`;
+        rombo.style.height = `${this.diagonalMayor}px`;
+        document.querySelector("#resultAreaRombo").innerHTML = `Area: ${area}`
+    }
 }
--- js/index.js
+++ js/index.js
@@ -18,3 +18,54 @@ function elevar(){
 
     result.innerHTML = `El valor resultate es ${resultado}`
 }
+
+function string(){
+    const txt1 = document.querySelector("#texto1").value;
+    const txt2 = document.querySelector("#texto2").value;
+    const txt3 = document.querySelector("#texto3").value;
+
+    let textResult = document.querySelector("#textResult");
+    textResult.innerHTML = `Texto 1: ${txt1},
+    Texto 2: ${txt2},
+    Texto 3: ${txt3}, `
+}
+
+function StringBuffer() {
+    this.__strings__ = new Array;
+}
+ 
+StringBuffer.prototype.append = function (str) {
+    this.__strings__.push(str);
+};
+ 
+StringBuffer.prototype.toString = function () {
+    return this.__strings__.join("");
+};
+
+function getStringBuffer(){
+    var buffer = new StringBuffer();
+    const txt1 = document.querySelector("#texto1").value;
+    const txt2 = document.querySelector("#texto2").value;
+    const txt3 = document.querySelector("#texto3").value;
+    buffer.append(txt1);
+    buffer.append(txt2);
+    buffer.append(txt3);
+    let result = buffer;
+    let textResult = document.querySelector("#textResult");
+    textResult.innerHTML = `Texto: ${result}`
+}
+
+function getStringTokenizer(){
+    var buffer = new StringBuffer();
+    const txt1 = document.querySelector("#texto1").value;
+    const txt2 = document.querySelector("#texto2").value;
+    const txt3 = document.querySelector("#texto3").value;
+    buffer.append(txt1);
+    buffer.append(txt2);
+    buffer.append(txt3);
+    let result = buffer.toString();
+    const separador = document.querySelector("#separador").value;
+
+    let textResult = document.querySelector("#textResult");
+    textResult.innerHTML = `Texto Completo: ${result}, Texto tokenizer: ${result.split(separador)}`
+}
